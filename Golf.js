fb0304a33dc0eb4ef041eeb374a6fcb6964a0433
Andrew Ribicki <aribicki@saintjoe.edu>
2015-03-25 08:41:24 -0500

    changed console output to println

--- Golf.js
+++ Golf.js
@@ -34,10 +34,10 @@ function verify(regexp, yes, no) {
   if (regexp.source == "...") return;
   yes.forEach(function(s) {
     if (!regexp.test(s))
-      console.log("Failure to match '" + s + "'");
+      println("Failure to match '" + s + "'");
   });
   no.forEach(function(s) {
     if (regexp.test(s))
-      console.log("Unexpected match for '" + s + "'");
+      println("Unexpected match for '" + s + "'");
   });
 }
